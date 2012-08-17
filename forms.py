f71b63ddc1414b57c1a5fafeb11b8a64c0ce692f
Ankush Chadda <ankush.chadda@gmail.com>
2012-08-17 16:04:11 +0530

    added forms, models

---
+++ forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from models import Place
+
+class LocationForm(forms.ModelForm):
+    class Meta:
+        model = Place
+             
+    place = forms.CharField(widget=forms.TextInput(attrs = {'class':'searchTextField'}))
+    city = forms.CharField(widget=forms.HiddenInput(attrs={'name':'city'}))
+    state = forms.CharField(widget=forms.HiddenInput(attrs={'name':'state'}))
+    country = forms.CharField(widget=forms.HiddenInput(attrs={'name':'country'}))
+    latitude = forms.CharField(widget=forms.HiddenInput(attrs={'name':'latitude'}))
+    longitude= forms.CharField(widget=forms.HiddenInput(attrs={'name':'longitude'}))
