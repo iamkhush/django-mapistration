f239d6f5f65665729605b460802ea3dac6eefdb6
Ankush Chadda <ankush.chadda@gmail.com>
2012-09-06 08:50:33 +0530

    Followed Pep8 guidelines

--- mapistration/models.py
+++ mapistration/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
+
 class Place(models.Model):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
     latitude = models.CharField(max_length=15)
     longitude = models.CharField(max_length=15)
-    
