2668af1c021c9a86198c64757465d7c6d862601d
Ankush Chadda <ankush.chadda@gmail.com>
2012-09-15 17:06:02 +0530

    Pep8 changes and added templates for including scripts and js

--- mapistration/forms.py
+++ mapistration/forms.py
@@ -6,15 +6,15 @@ class LocationForm(forms.ModelForm):
     class Meta:
         model = Place
 
-    place = forms.CharField(widget=forms.TextInput(
-                            attrs={'class': 'searchTextField'}))
-    city = forms.CharField(widget=forms.HiddenInput(
-                            attrs={'name': 'city'}))
-    state = forms.CharField(widget=forms.HiddenInput(
-                            attrs={'name': 'state'}))
-    country = forms.CharField(widget=forms.HiddenInput(
-                            attrs={'name': 'country'}))
-    latitude = forms.CharField(widget=forms.HiddenInput(
-                            attrs={'name': 'latitude'}))
-    longitude = forms.CharField(widget=forms.HiddenInput(
-                            attrs={'name': 'longitude'}))
+    place = forms.CharField(widget=forms.TextInput(attrs={
+                                'class': 'searchTextField'}))
+    city = forms.CharField(widget=forms.HiddenInput(attrs={
+                    'name': 'city'}), required=False)
+    state = forms.CharField(widget=forms.HiddenInput(attrs={
+                    'name': 'state'}), required=False)
+    country = forms.CharField(widget=forms.HiddenInput(attrs={
+                    'name': 'country'}), required=False)
+    latitude = forms.CharField(widget=forms.HiddenInput(attrs={
+                    'name': 'latitude'}), required=False)
+    longitude = forms.CharField(widget=forms.HiddenInput(attrs={
+                    'name': 'longitude'}), required=False)
